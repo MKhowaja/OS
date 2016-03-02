27678eb1ede2954a5a7b62c1a699c586e31180b2
Jeff Zhang <a920249925@gmail.com>
2016-03-02 15:08:31 -0500

    interrupt process & io process init

---
+++ P2/src/i_proc.c
@@ -0,0 +1,15 @@
+//execute every hardware interrupts
+void timer_i_process(){
+	//increment time
+
+	//while loop to retrieve new request messages, add to sorted order list
+
+	//check timeout, and send to sender process	
+}
+
+//every keystroke
+// 1.respond msg from crt, -> serial port 
+// 2.get character, forward to kcd
+void uart_i_process(){
+
+}

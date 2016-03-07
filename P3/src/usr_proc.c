0dd6681b520c0aba35eb100175ab35fdc23c48cd
unknown <mustaqeem.khowaja@gmail.com>
2016-03-07 12:17:38 -0500

    Moved new set priority proc and test proc to test set priority proc to new file and reverted old usr proc file

--- P3/src/usr_proc.c
+++ P3/src/usr_proc.c
@@ -20,23 +20,14 @@ void set_test_procs() {
 		g_test_procs[i].m_stack_size=0x200;
 		passed_test[i] = 0;
 	}
-	//passed_test[5] = 1;
-  
-	g_test_procs[0].m_pid = PID_SET_PRIO;
-	g_test_procs[0].mpf_start_pc = &set_process_proc;
-	g_test_procs[0].m_priority   = HIGH;
-    
-    g_test_procs[1].m_pid = PID_P1;
-	g_test_procs[1].mpf_start_pc = &test_proc_for_set_process_proc;
-	g_test_procs[1].m_priority   = LOWEST;
-	/*
+	passed_test[5] = 1;	
 	g_test_procs[0].m_pid = PID_P1;  ;
 	g_test_procs[0].mpf_start_pc = &proc_p2_1;
 	g_test_procs[0].m_priority   = HIGH;
 	
 	g_test_procs[1].m_pid = PID_P2;
 	g_test_procs[1].mpf_start_pc = &proc_p2_2;
-	g_test_procs[1].m_priority   = MEDIUM; 
+	g_test_procs[1].m_priority   = MEDIUM;
 	
 	g_test_procs[2].m_pid = PID_P3;
 	g_test_procs[2].mpf_start_pc = &proc_p2_3;
@@ -53,121 +44,12 @@ void set_test_procs() {
 	g_test_procs[5].m_pid= PID_P6;
 	g_test_procs[5].mpf_start_pc = &proc_p2_6;
 	g_test_procs[5].m_priority   = LOWEST;
-	*/
+	
+	//g_test_procs[6].m_pid = PID_CLOCK;
+  //g_test_procs[6].m_priority = HIGH;
+  //g_test_procs[6].mpf_start_pc = &clock_proc;
 	
 	uart1_init();
-}
-
-void set_process_proc(void){
-	int i;
-	int pid;
-    int space_loc;
-    int priority;
-	MSG_BUF* read_msg;
-    MSG_BUF* msg;//send out
-	int sender_id;
-	const int buf_size = 100;
-	char buf[buf_size];
-	char cmd[3] = "%C";
-    char buf_to_int [3];
-
-    msg = (MSG_BUF*)request_memory_block();
-    //reg cmd
-    msg->mtype = KCD_REG;
-    strncpy(msg->mtext, cmd, strlen(cmd)+1);
-    send_message(PID_KCD, msg);
-    while (1){
-        for(i = 0; i < buf_size; i++){
-            buf[i] = '\0';
-        }
-        read_msg = receive_message(&sender_id);
-		strncpy(buf, read_msg->mtext, buf_size);
-		release_memory_block(read_msg);
-		if( strlen(buf) < 8 || buf[0] != cmd[0] ||  buf[1] != cmd[1] || buf[2] != ' '){
-            //do nothing, next
-            #ifdef DEBUG_0
-				printf("ERROR! Set Process Proc got invalid message\r\n");
-			#endif /* DEBUG_0 */
-			continue;
-		}
-        if (buf[4] == ' '){
-            space_loc = 4;
-            if (buf[3] < 48 || buf[3] > 57) { //NaN
-                #ifdef DEBUG_0
-                    printf("ERROR! Set Process Proc got NaN for PID\r\n");
-                #endif /* DEBUG_0 */
-                continue;
-            }
-            buf_to_int[0] = buf[3];
-            buf_to_int[1] = '\0';
-        }
-        else {
-            space_loc = 5;
-            if (buf[3] < 48 || buf[3] > 57) { //NaN
-                #ifdef DEBUG_0
-                    printf("ERROR! Set Process Proc got NaN for PID\r\n");
-                #endif /* DEBUG_0 */
-                continue;
-            }
-            buf_to_int[0] = buf[3];
-            if (buf[4] < 48 || buf[4] > 57) { //NaN
-                #ifdef DEBUG_0
-                    printf("ERROR! Set Process Proc got NaN for PID\r\n");
-                #endif /* DEBUG_0 */
-                continue;
-            }
-            buf_to_int[1] = buf[4];
-            buf_to_int[2] = '\0';
-        }
-        if (space_loc+2 != strlen(buf)-2){
-            #ifdef DEBUG_0
-				printf("ERROR! Set Process Proc got invalid chars\r\n");
-			#endif /* DEBUG_0 */
-			continue;
-        }
-        pid = atoi(buf_to_int);
-        if (pid < 1 || pid > 13){ //should not allow change priority of NULL or I PROCS
-            #ifdef DEBUG_0
-				printf("ERROR! Set Process Proc got restricted pid\r\n");
-			#endif /* DEBUG_0 */
-			continue;
-        }
-        if (buf[space_loc] != ' '){
-            #ifdef DEBUG_0
-				printf("ERROR! Set Process Proc got invalid message\r\n");
-			#endif /* DEBUG_0 */
-			continue;
-        }
-        if (buf[space_loc+1] < 48 || buf[space_loc+1] > 57) { //NaN
-            #ifdef DEBUG_0
-                printf("ERROR! Set Process Proc got NaN for Priority\r\n");
-            #endif /* DEBUG_0 */
-            continue;
-        }
-        buf_to_int[0] = buf[space_loc + 1];
-        buf_to_int[1] = '\0';
-        priority = atoi(buf_to_int);
-        if (priority < 0 || priority > 3){
-            #ifdef DEBUG_0
-                printf("ERROR! Set Process Proc got Invalid Priority\r\n");
-            #endif /* DEBUG_0 */
-            continue;
-        }
-        set_process_priority (pid, priority);
-	}
-}
-
-void test_proc_for_set_process_proc(void){
-    int i;
-    while (1){
-        if (i%20 == 0){
-            #ifdef DEBUG_0
-                printf("Test Proc has Priority: %d\r\n", get_process_priority(PID_P1));
-            #endif /* DEBUG_0 */
-        }
-        i++;
-        i%=20;
-    }
 }
 
 /*
@@ -180,6 +62,7 @@ release msg
 set priority to lowest
 context switch
 */
+
 void proc_p2_1(void) {
 	MSG_BUF *msg = NULL;
 	char send_msg[6] = "Blah1";
@@ -208,7 +91,7 @@ void proc_p2_1(void) {
 	
 	release_memory_block(msg);
 	msg = NULL;
-	//set_process_priority(PID_P1, LOWEST);
+	set_process_priority(PID_P1, LOWEST);
 	if(strcmp(receive_msg,"Blah4") == 0 && sender_id == PID_P4){
 			passed_test[0] = 1;
 	}
@@ -294,7 +177,7 @@ void proc_p2_3(void) {
 	set_process_priority(PID_P3, LOWEST);
 	
 	if(strcmp(receive_msg,"Blah1") == 0 && sender_id == PID_P1){
-//			passed_test[2] = 1;
+			passed_test[2] = 1;
 	}
 	
 	while(1) {
@@ -335,7 +218,7 @@ void proc_p2_4(void) {
 	set_process_priority(PID_P4, LOWEST);
 	
 	if(strcmp(receive_msg,"Blah3") == 0 && sender_id == PID_P3){
-//			passed_test[3] = 1;
+			passed_test[3] = 1;
 	}
 	
 	while(1) {
@@ -366,7 +249,7 @@ void proc_p2_5(void) {
 	set_process_priority(PID_P5, LOWEST);	
 	
 	if(ret_val == RTX_OK){
-			//passed_test[4] = 1;
+			passed_test[4] = 1;
 	}
 	
 	while(1) {
@@ -406,4 +289,3 @@ void proc_p2_6(void) {
 		}
     
 }
-
